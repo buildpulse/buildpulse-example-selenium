9db2613111599ea3f323e2fb50d79e7927687ee0
buildpulser <support@buildpulse.io>
2026-08-13 12:09:55 -0700

    tests: modernize to Selenium 4 (headless Chrome via setup-chrome, drop autoinstaller/xvfb)

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,13 +1,21 @@
+import os
 import unittest
-from selenium import webdriver
-import chromedriver_autoinstaller
-import xmlrunner
 
-chromedriver_autoinstaller.install()
+import xmlrunner
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 class ExampleTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        options = Options()
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        chrome_bin = os.environ.get("CHROME_BIN")
+        if chrome_bin:
+            options.binary_location = chrome_bin
+        self.driver = webdriver.Chrome(options=options)
 
     def test_page_title(self):
         self.driver.get("http://example.com")
@@ -16,6 +24,6 @@ class ExampleTest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-if __name__ == '__main__':
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='./test-reports'))
 
+if __name__ == "__main__":
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output="./test-reports"))
